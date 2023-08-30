13ee1c09f92e362967216413fb032507883f2508
kgharami <kgharami@mymedicalhub.com>
2023-08-31 00:36:43 +0600

    db setting updated

--- ipe/settings.py
+++ ipe/settings.py
@@ -74,12 +74,24 @@ AUTH_USER_MODEL = "app.MyCustomUser"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if env("DB") == 'postgres':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': env("db_name"),             # myproject
+            'USER': env("db_user"),             # myprojectuser
+            'PASSWORD': env("db_password"),     # password
+            'HOST': 'localhost',
+            'PORT': '',
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
